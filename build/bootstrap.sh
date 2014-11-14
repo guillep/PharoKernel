642ac58f51d32f6574812a4f96713436d73efb30
GuillePolito <guillermopolito@gmail.com>
2014-11-14 12:12:11 +0100

    added temp declaration in doit to avoid warning

--- build/bootstrap.sh
+++ build/bootstrap.sh
@@ -34,7 +34,7 @@ REPO=http://smalltalkhub.com/mc/Guille/Seed/main
 
 echo "Configuration Loaded. Opening script..."
 
-echo -e "
+echo -e "|objectSpace|
 objectSpace := PharoKernelBuilder2 bootstrap.
 objectSpace serializeInFileNamed: 'PharoKernel.image'.
 Smalltalk snapshot: false andQuit: true.
